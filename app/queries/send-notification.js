b4f8782739e548f6de1babc38948f9aeda2fb437
Rana Salem <ranatasalem@gmail.com>
2024-06-19 10:45:33 +0100

    update sendNotification to only send when internalUser is set to true

--- app/queries/send-notification.js
+++ app/queries/send-notification.js
@@ -4,17 +4,23 @@ const NotifyClient = require('notifications-node-client').NotifyClient
 const sendNotification = async (message) => {
   const notifyClient = new NotifyClient(process.env.NOTIFY_API_KEY)
 
-  await notifyClient.sendEmail(
-    process.env.NOTIFY_TEMPLATE_ID,
-    process.env.NOTIFY_TEST_EMAIL,
-    {
-      personalisation: {
-        heading: message.body.heading,
-        content: message.body.body
-      },
-      reference: uuidv4()
-    }
-  )
+  if (message.body.internalUser) {
+    await notifyClient.sendEmail(
+      process.env.NOTIFY_TEMPLATE_ID,
+      process.env.NOTIFY_TEST_EMAIL,
+      {
+        personalisation: {
+          heading: message.body.heading,
+          content: message.body.body
+        },
+        reference: uuidv4()
+      }
+    )
+
+    console.log('Notify has sent an email to the external user: internalUser is set to true.')
+  } else {
+    console.log('Notify did not send email: internalUser is false.')
+  }
 }
 
 module.exports = { sendNotification }
