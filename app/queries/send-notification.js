284c5a5447c2b09387d910e4d3d4f0837113df43
Rana Salem <ranatasalem@gmail.com>
2024-07-11 11:05:39 +0100

    wip

--- app/queries/send-notification.js
+++ app/queries/send-notification.js
@@ -1,10 +1,10 @@
-const { v4: uuidv4 } = require('uuid')
+// const { v4: uuidv4 } = require('uuid')
 const NotifyClient = require('notifications-node-client').NotifyClient
 
 const sendNotification = async (message) => {
   const notifyClient = new NotifyClient(process.env.NOTIFY_API_KEY)
 
-  if (message.body.internalUser) {
+  if (message.body.responses.internalUser) {
     await notifyClient.sendEmail(
       process.env.NOTIFY_TEMPLATE_ID,
       process.env.NOTIFY_TEST_EMAIL,
@@ -13,13 +13,14 @@ const sendNotification = async (message) => {
           heading: message.body.responses.heading,
           content: message.body.responses.body
         },
-        reference: uuidv4()
+        reference: message.body.id
       }
     )
 
-    console.log('Notify has sent an email to the external user: internalUser is set to true.')
+    console.log('Notify has sent an email notification: internalUser is true.')
   } else {
-    console.log('Notify did not send email: internalUser is false.')
+    console.log('Notify has not sent an email notification: internalUser is false.')
+    console.log(message.body.responses[0].internalUser)
   }
 }
 
